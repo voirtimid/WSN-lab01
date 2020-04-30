4855a6336daa5267f18a200d84421af2e9366c0f
voirtimid <=>
2020-04-30 17:58:52 +0200

    updates for lab02 plots

--- lab02.py
+++ lab02.py
@@ -8,26 +8,34 @@ humidity_data, humidity_data_2day, humidity_data_3day = read_data("humidity_data
 temperature_thresholds = [round(i, 1) for i in range(10)]
 humidity_thresholds = [round(i, 1) for i in range(10)]
 
-mse_errors, reduced_transmission = dual_prediction_schema(temperature_data,
-                                                          humidity_data, 1,
-                                                          temperature_thresholds,
-                                                          humidity_thresholds)
 
-mse_errors_2day, reduced_transmission_2day = dual_prediction_schema(temperature_data_2day,
-                                                                    humidity_data_2day, 1,
-                                                                    temperature_thresholds,
-                                                                    humidity_thresholds)
+def plot(moving_average_factor, label):
+    mse_errors, reduced_transmission = dual_prediction_schema(temperature_data,
+                                                              humidity_data,
+                                                              moving_average_factor,
+                                                              temperature_thresholds,
+                                                              humidity_thresholds)
 
-mse_errors_3day, reduced_transmission_3day = dual_prediction_schema(temperature_data_3day,
-                                                                    humidity_data_3day, 1,
-                                                                    temperature_thresholds,
-                                                                    humidity_thresholds)
+    mse_errors_2day, reduced_transmission_2day = dual_prediction_schema(temperature_data_2day,
+                                                                        humidity_data_2day,
+                                                                        moving_average_factor,
+                                                                        temperature_thresholds,
+                                                                        humidity_thresholds)
 
-# alt shift e
+    mse_errors_3day, reduced_transmission_3day = dual_prediction_schema(temperature_data_3day,
+                                                                        humidity_data_3day,
+                                                                        moving_average_factor,
+                                                                        temperature_thresholds,
+                                                                        humidity_thresholds)
 
-scatter_plot(mse_errors, "MSE Errors Daily")
-scatter_plot(reduced_transmission, "Reduced Transmission Daily")
-scatter_plot(mse_errors_2day, "MSE Errors Every Second Day")
-scatter_plot(reduced_transmission_2day, "Reduced Transmission Every Second Day")
-scatter_plot(mse_errors_3day, "MSE Errors Every Third Day")
-scatter_plot(reduced_transmission_3day, "Reduced Transmission Every Third Day")
+    scatter_plot(mse_errors, f"MSE Errors Daily - {label}")
+    scatter_plot(reduced_transmission, f"Reduced Transmission Daily - {label}")
+    scatter_plot(mse_errors_2day, f"MSE Errors Every Second Day - {label}")
+    scatter_plot(reduced_transmission_2day, f"Reduced Transmission Every Second Day - {label}")
+    scatter_plot(mse_errors_3day, f"MSE Errors Every Third Day - {label}")
+    scatter_plot(reduced_transmission_3day, f"Reduced Transmission Every Third Day - {label}")
+
+
+plot(1, "Moving Average 1")
+plot(2, "Moving Average 2")
+plot(3, "Moving Average 3")
